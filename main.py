36dd55ee140dd46ae5881b535b4ca49170253cba
ActActAct <suliu@umich.edu>
2023-09-13 22:31:33 -0400

    Completed first GET request of spotify api

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import requests
 
 url = "https://accounts.spotify.com/api/token"
-
 headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
-
 data = {
     "grant_type": "client_credentials",
     "client_id": "5972487482b145d183008a0ff7af682e",
@@ -13,6 +11,12 @@ data = {
 }
 
 response = requests.post(url, headers=headers, data=data)
+access_token_json = response.json()
+access_token = access_token_json['access_token']
+header = {'Authorization': 'Bearer ' + access_token}
 
-# Print response
-print(response.json())
+
+playlist_link = 'https://api.spotify.com/v1/playlists/7gmLrKkw6OMl70O5Y2CEtM?market=US'
+playlist_response = requests.get(playlist_link, headers=header)
+
+print(playlist_response.json())
